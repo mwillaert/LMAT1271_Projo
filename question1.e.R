8eb9272519cdfa9041d36faa37a6c2460b075971
Maxime Willaert <maximewillaert@AItof-web-ship.local>
2022-04-06 17:53:24 +0200

    q1.e no comment

--- question1.e.R
+++ question1.e.R
@@ -4,5 +4,29 @@ qdist<-function(t,theta_1,theta_2){
 }
 
 rdist<-function(smpl_size,theta_1,theta_2){
-  
+  U<-runif(smpl_size)
+  result<-c()
+  for(i in 1:smpl_size){
+    result[i]<-qdist(U[i],theta_1,theta_2)
+  }
+  return(result)
 }
+
+ddist<-function(x,theta_1,theta_2){
+  result<-theta_1 * (theta_2^theta_1)/(x^(theta_1+1))
+  return(result)
+}
+
+t1<-10
+t2<-0.3
+n<-1e5
+smpl<-rdist(n,t1,t2)
+h<-hist(smpl,breaks=50,freq = FALSE)
+xh<-h$mids
+yh<-c()
+for (i in 1:length(xh)){
+  yh[i]<-ddist(xh[i],t1,t2)
+}
+
+lines(xh,yh)
+abline(v=t2)
