e01fe81245b129cfcfe2c918c223ffd66c6b814e
Maxime Willaert <maximewillaert@AItof-web-ship.local>
2022-04-14 16:38:00 +0200

    comments

--- question1.e.R
+++ question1.e.R
@@ -1,8 +1,22 @@
+#Fonction calculant le quantile Q(t) pour la distribution de l'enonce
+#Input:
+#   t (float): variable de la fonction quantile Q(t)
+#   theta_1,2 (float): parametres de la distribution
+#Output:
+#   float: quantile Q(t)
+
 qdist<-function(t,theta_1,theta_2){
   result <- theta_2 * (1-t)^(-1/theta_1)
   return(result)
 }
 
+#Fonction permettant de generer un echantillon iid de taille n suivant
+#la distribution de l'enonce
+#Input:
+#   smpl_size (int): taille de l'echantillon
+#   theta_1,2 (float): parametres de la distribution
+#Output:
+#   vector of floats: echantillon iid (x1,x2,...,xn)
 rdist<-function(smpl_size,theta_1,theta_2){
   U<-runif(smpl_size)
   result<-c()
@@ -12,11 +26,18 @@ rdist<-function(smpl_size,theta_1,theta_2){
   return(result)
 }
 
+#Fonction donnant la densite de probabilite de l'enonce
+#Input:
+#   x (float): variable de la dentite f(x)
+#   theta_1,2 (float): parametres de la distribution
+#Ouput:
+#   float: densite au point x
 ddist<-function(x,theta_1,theta_2){
   result<-theta_1 * (theta_2^theta_1)/(x^(theta_1+1))
   return(result)
 }
 
+#Exemples
 t1<-10
 t2<-0.3
 n<-1e5
