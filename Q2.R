43f048ed0ce59a340763301dc8d5ee540a3ff9ef
Maxime Willaert <maximewillaert@AItof-web-ship.local>
2022-05-17 10:57:27 +0200

    renaming

---
+++ Q2.R
@@ -0,0 +1,35 @@
+#Importe fonctions
+source("toolbox.R")
+
+#Lecture du fichier
+Data<-read.delim("Data.txt",sep=";")
+x<-seq(5,47,length.out=100)
+
+#Affichage de différents couples X*, Y*
+plot(Data$X,Data$Y)
+plot(Data$X^(-1),Data$Y)
+plot(Data$X^(-2),Data$Y)
+plot(Data$X^(-3),Data$Y)
+plot(Data$X^(-4),Data$Y)
+#relation semble "la plus linéaire" pour Y*=Y et X=X^-2
+
+#Calcul des paramètres de régression linéaire
+par<-lin_reg_parameters(Data$X^(-1),Data$Y)
+par2<-lin_reg_parameters(Data$X^(-2),Data$Y)
+par3<-lin_reg_parameters(Data$X^(-3),Data$Y)
+
+plot(Data$X^(-1),Data$Y)
+lines(Data$X^(-1),Data$X^(-1)*par[2]+par[1])
+
+plot(Data$X^(-2),Data$Y)
+lines(Data$X^(-2),Data$X^(-2)*par2[2]+par2[1])
+
+#Affichage des résultats
+print(par)
+print(par2)
+print(par3)
+
+#Résultats (b0, b1, R2, p-value) avec Y*=b0+b1*X*
+#Pour X*=1/X et Y*=Y:  2.717689e+01 -6.295179e+01  4.841859e-01  6.210726e-07
+#Pour X*=1/X^2 et Y*=Y: 2.300021e+01 -2.038217e+02  4.670521e-01  1.174551e-06
+
